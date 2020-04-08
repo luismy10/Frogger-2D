6aaf5835837482f17df25ee8db6314632859ad52
Rahamsis Correa Gamarra <mirhaslove_24@hotmail.com>
2020-04-08 17:01:12 -0500

    subiendo cambios

--- src/entity/Modelo.java
+++ src/entity/Modelo.java
@@ -3,6 +3,7 @@ package entity;
 import game.WindowCanvas;
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.Rectangle;
 import javax.swing.ImageIcon;
 
 public class Modelo {
@@ -13,14 +14,16 @@ public class Modelo {
     protected int width;
     protected int height;
     protected int speedplus;
+    protected String direction;
 
-    public Modelo(int x, int y, String ruta, int width, int height, int speedplus) {
+    public Modelo(int x, int y, String ruta, int width, int height, int speedplus, String direction) {
         imagen = new ImageIcon(getClass().getResource(ruta)).getImage();
         this.width = width;
         this.height = height;
         this.x = x;
         this.y = y;
         this.speedplus = speedplus;
+        this.direction = direction;
     }
 
     public Modelo(int x, int y, int width, int height, int speedplus) {
@@ -32,20 +35,16 @@ public class Modelo {
     }
 
     public void update(double delta) {
-
-    }
-
-    public void moveleft() {
-        x -= speedplus;
-        if (x < -width) {
-            x = WindowCanvas.WIDTHCANVAS + 50;
-        }
-    }
-
-    public void moverigth() {
-        x += speedplus;
-        if (x > WindowCanvas.WIDTHCANVAS + 50) {
-            x = -width;
+        if (direction.equalsIgnoreCase("left")) {
+            x -= speedplus;
+            if (x < -width) {
+                x = WindowCanvas.WIDTHCANVAS + 50;
+            }
+        } else if (direction.equalsIgnoreCase("right")) {
+            x += speedplus;
+            if (x > WindowCanvas.WIDTHCANVAS + 50) {
+                x = -width;
+            }
         }
     }
 
@@ -56,5 +55,15 @@ public class Modelo {
     public void render(Graphics2D g2d, Image turtle) {
         g2d.drawImage(turtle, x, y, width, height, null);
     }
+
+    public Rectangle getRectangle() {
+        return new Rectangle(x, y, width, height);
+    }
+
+    public int getSpeedplus() {
+        return speedplus;
+    }
+
+    
 
 }
--- src/entity/Player.java
+++ src/entity/Player.java
@@ -3,12 +3,13 @@ package entity;
 import game.WindowCanvas;
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.Rectangle;
 import java.awt.event.KeyEvent;
 import static java.awt.event.KeyEvent.VK_DOWN;
 import static java.awt.event.KeyEvent.VK_LEFT;
 import static java.awt.event.KeyEvent.VK_RIGHT;
 import static java.awt.event.KeyEvent.VK_UP;
-import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import javax.swing.ImageIcon;
@@ -17,21 +18,26 @@ import util.Keyboard;
 public class Player {
 
     private enum FrogState {
-        CALM, MOVE
+        CALM, MOVE, DIED
     }
 
     private FrogState frogState = FrogState.CALM;
-   
+
     private int x;
     private int y;
     private final int w;
     private final int h;
     private final int speed;
 
-    private Map<String, Image[]> animations;
+    private final Map<String, Image[]> animations;
     public Image[] frames;
     private int frameIndex;
     private int posxAnimation;
+
+    private ArrayList<Modelo> carros;
+
+    private Modelo tronquitos;
+    private River river;
 
     public Player(int width, int height) {
         x = (WindowCanvas.WIDTHCANVAS - width) / 2;
@@ -41,21 +47,33 @@ public class Player {
         speed = 32;
 
         animations = new HashMap<>();
-        setAnimation("frog_up_",(short)3);
-        setAnimation("frog_down_",(short)3);
-        setAnimation("frog_left_",(short)3);
-        setAnimation("frog_right_",(short)3);
-        
+        setAnimation("frog_up_", (short) 3);
+        setAnimation("frog_down_", (short) 3);
+        setAnimation("frog_left_", (short) 3);
+        setAnimation("frog_right_", (short) 3);
+        setAnimation("frog_dying_", (short) 4);
+
         frames = animations.get("frog_up_");
-        
+
         frameIndex = 0;
         posxAnimation = 0;
     }
-    
-    private void setAnimation(String key, short value ){
+
+    public void setModeloCarros(ArrayList modelos) {
+        this.carros = modelos;
+    }
+
+    public void setModeloTronquito(Modelo modelo) {
+        this.tronquitos = modelo;
+    }
+    public void setRiver(River river){
+        this.river = river;
+    }
+
+    private void setAnimation(String key, short value) {
         Image[] image = new Image[value];
-        for(short i = 0; i < image.length; i++){
-            image[i] = new ImageIcon(getClass().getResource("/image/"+key+i+".png")).getImage();
+        for (short i = 0; i < image.length; i++) {
+            image[i] = new ImageIcon(getClass().getResource("/image/" + key + i + ".png")).getImage();
         }
         animations.put(key, image);
     }
@@ -69,24 +87,42 @@ public class Player {
     public void update(double delta) {
         switch (frogState) {
             case CALM:
-if (Keyboard.isKeyPressed(VK_LEFT)) {
-            move(-1, 0);
-        }
-        else if (Keyboard.isKeyPressed(VK_RIGHT)) {
-            move(1, 0);
-        }
-        if (Keyboard.isKeyPressed(VK_UP)) {
-            move(0, -1);
-        }
-        else if (Keyboard.isKeyPressed(VK_DOWN)) {
-            move(0, 1);
-        }
+                if (Keyboard.isKeyPressed(VK_LEFT)) {
+                    move(-1, 0);
+                } else if (Keyboard.isKeyPressed(VK_RIGHT)) {
+                    move(1, 0);
+                }
+                if (Keyboard.isKeyPressed(VK_UP)) {
+                    move(0, -1);
+                } else if (Keyboard.isKeyPressed(VK_DOWN)) {
+                    move(0, 1);
+                }
+
+                for (Modelo modelo1 : carros) {
+                    if (collision(getRectangle(), modelo1.getRectangle())) {
+                        changeFrogState(FrogState.DIED);
+                        frameIndex = 0;
+                        setMovimiento("frog_dying_");
+                    }
+                }
+
+                if (collision(getRectangle(), tronquitos.getRectangle())) {
+                    x += tronquitos.getSpeedplus();
+                }
+                if(collision(getRectangle(), river.getRectangle())){
+                    changeFrogState(frogState.DIED);
+                    setMovimiento("frog_dying_");
+                }
+
                 break;
             case MOVE:
-
                 updateJumping();
                 break;
+            case DIED:
+                updatedDied();
+                break;
         }
+
     }
 
     public void move(int vx, int vy) {
@@ -99,16 +135,42 @@ if (Keyboard.isKeyPressed(VK_LEFT)) {
     }
 
     private void updateJumping() {
-        frameIndex++;        
-        int frame = (int) (frameIndex / 3.5);
-        System.out.println(frame);
+        frameIndex++;
+        int frame = (int) (frameIndex / 4);
         posxAnimation = frame == 3 ? 0 : frame;
         if (frame > 2.5) {
             changeFrogState(FrogState.CALM);
         }
     }
-    
-    private void setMovimiento(String value){
+
+    private void updatedDied() {
+        frameIndex++;
+        int frame = (int) (frameIndex / 15);
+        posxAnimation = frame == 4 ? 0 : frame;
+        if (frame > 3.5) {
+            changeFrogState(FrogState.CALM);
+            reset();
+        }
+    }
+
+    private void reset() {
+        x = (WindowCanvas.WIDTHCANVAS - w) / 2;
+        y = 226 * 2;
+        frames = animations.get("frog_up_");
+    }
+
+    public Rectangle getRectangle() {
+        return new Rectangle(x, y, w, h);
+    }
+
+    private boolean collision(Rectangle src, Rectangle des) {
+        return src.x < des.x + des.width
+                && src.x + src.width > des.x
+                && src.y < des.y + des.height
+                && src.height + src.y > des.y;
+    }
+
+    private void setMovimiento(String value) {
         frames = animations.get(value);
     }
 
@@ -117,14 +179,21 @@ if (Keyboard.isKeyPressed(VK_LEFT)) {
     }
 
     public void keypressed(KeyEvent e) {
-        if (e.getKeyCode() == KeyEvent.VK_UP) {
-            move(0, -1);   
-        } else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
-            move(0, 1);
-        } else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-            move(-1, 0);
-        } else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-            move(1, 0);
+        switch (e.getKeyCode()) {
+            case KeyEvent.VK_UP:
+                move(0, -1);
+                break;
+            case KeyEvent.VK_DOWN:
+                move(0, 1);
+                break;
+            case KeyEvent.VK_LEFT:
+                move(-1, 0);
+                break;
+            case KeyEvent.VK_RIGHT:
+                move(1, 0);
+                break;
+            default:
+                break;
         }
     }
 
